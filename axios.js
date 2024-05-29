1b9fa67822078d5086b46ce612549a60ab6e4f54
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-05-29 15:03:38 -0500

    using async and await key words to fetch http requests

--- axios.js
+++ axios.js
@@ -1,7 +1,12 @@
-axios.get("https://swapi.dev/api/people/1/")
+/*axios.get("https://swapi.dev/api/people/1/")
 .then (res => {
     console.log("Response:0", res)
 })
 .catch ((e) => {
     console.log("Error", e);
-});
+});*/
+
+const getStarWarsPerson = async () => {
+    const res = await axios.get("https://swapi.dev/api/people/1/");
+    console.log(res);
+};
