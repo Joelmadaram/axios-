013add30746a2ef965c0f2428bba512614becf70
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-05-29 15:09:57 -0500

    used try and catch to find errors

--- axios.js
+++ axios.js
@@ -7,6 +7,11 @@
 });*/
 
 const getStarWarsPerson = async (id) => {
+try {
     const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
     console.log(res);
+} 
+catch (e) {
+    console.log("error!!",e);
+}
 };
