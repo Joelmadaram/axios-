97806fe770a573a5aac96e9058a1bccd64248485
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-05-29 18:08:28 -0500

    used two different methods working with axios. response and reject method, async and await method.

--- axios.js
+++ axios.js
@@ -1,11 +1,13 @@
-/*axios.get("https://swapi.dev/api/people/1/")
+axios.get("https://swapi.dev/api/people/1/")
 .then (res => {
     console.log("Response:0", res)
 })
 .catch ((e) => {
     console.log("Error", e);
-});*/
+});
 
+/*used two different methods working with axios.
+response and reject method, async and await method.*/
 const getStarWarsPerson = async (id) => {
 try {
     const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
